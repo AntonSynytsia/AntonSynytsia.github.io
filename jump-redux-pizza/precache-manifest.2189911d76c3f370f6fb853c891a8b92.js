f060d80516685da36cb08bedd2e1d8c66feb3a65
AntonSynytsia <anton.synytsia@gmail.com>
2020-03-23 02:08:41 -0700

    Updated content

---
+++ jump-redux-pizza/precache-manifest.2189911d76c3f370f6fb853c891a8b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c2535cd1aaadee7542c70c3fec1e71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0da8611285906cecb6eb",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e76d4b58a9cef8d384cd",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0da8611285906cecb6eb",
+    "url": "./static/js/2.53610d6d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.53610d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76d4b58a9cef8d384cd",
+    "url": "./static/js/main.676632c1.chunk.js"
+  },
+  {
+    "revision": "ebc0353ede0d79edbeb9",
+    "url": "./static/js/runtime-main.544b41fd.js"
+  }
+]);
